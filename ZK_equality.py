81b8c9a5cf0fcaa6f91a0c826f4dd300e9a563fa
SpeedyCloud0-0 <63207198+SpeedyCloud0-0@users.noreply.github.com>
2021-07-27 11:13:48 -0700

    test

--- ZK_equality.py
+++ ZK_equality.py
@@ -14,8 +14,11 @@ def ZK_equality(G, H):
     D2 = r2.value * H + m * G
 
     # Generate a NIZK proving equality of the plaintexts
+    print("generation done")
     stmt = DLRep(C2 - r1 * H, D2 - r2 * H)
+    print("statement done")
     zk_proof = stmt.prove()
+
 
     # Return two ciphertexts and the proof
     return (C1, C2), (D1, D2), zk_proof
